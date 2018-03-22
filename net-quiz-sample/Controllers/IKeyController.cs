d8b76db769c4421d59990ef53c92a78591b54d92
Chudi Huang <chuhua@microsoft.com>
2018-03-22 16:58:00 +0800

    Optimize front-end appinsight injection

--- net-quiz-sample/Controllers/IKeyController.cs
+++ net-quiz-sample/Controllers/IKeyController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,15 +9,15 @@ namespace net_quiz_sample.Controllers
 {
     public class IKeyController : Controller
     {
-        public static String JavascriptInjectionPattern = "var appInsights=window.appInsights||function(a){function b(a){c[a]=function(){var b=arguments;c.queue.push(function(){c[a].apply(c,b)})}}var c={config:a},d=document,e=window;setTimeout(function(){var b=d.createElement(\"script\");b.src=a.url||\"https://az416426.vo.msecnd.net/scripts/a/ai.0.js\",d.getElementsByTagName(\"script\")[0].parentNode.appendChild(b)});try{c.cookie=d.cookie}catch(a){}c.queue=[];for(var f=[\"Event\",\"Exception\",\"Metric\",\"PageView\",\"Trace\",\"Dependency\"];f.length;)b(\"track\"+f.pop());if(b(\"setAuthenticatedUserContext\"),b(\"clearAuthenticatedUserContext\"),b(\"startTrackEvent\"),b(\"stopTrackEvent\"),b(\"startTrackPage\"),b(\"stopTrackPage\"),b(\"flush\"),!a.disableExceptionTracking){f=\"onerror\",b(\"_\"+f);var g=e[f];e[f]=function(a,b,d,e,h){var i=g&&g(a,b,d,e,h);return!0!==i&&c[\"_\"+f](a,b,d,e,h),i}}return c；}({instrumentationKey: \"{0}\"});window.appInsights=appInsights,appInsights.queue&&0===appInsights.queue.length&&appInsights.trackPageView();";
+        public static String JavascriptInjectionPattern = "var appInsights=window.appInsights||function(a){{function b(a){{c[a]=function(){{var b=arguments;c.queue.push(function(){{c[a].apply(c,b)}})}}}}var c={{config:a}},d=document,e=window;setTimeout(function(){{var b=d.createElement(\"script\");b.src=a.url||\"https://az416426.vo.msecnd.net/scripts/a/ai.0.js\",d.getElementsByTagName(\"script\")[0].parentNode.appendChild(b)}});try{{c.cookie=d.cookie}}catch(a){{}}c.queue=[];for(var f=[\"Event\",\"Exception\",\"Metric\",\"PageView\",\"Trace\",\"Dependency\"];f.length;)b(\"track\"+f.pop());if(b(\"setAuthenticatedUserContext\"),b(\"clearAuthenticatedUserContext\"),b(\"startTrackEvent\"),b(\"stopTrackEvent\"),b(\"startTrackPage\"),b(\"stopTrackPage\"),b(\"flush\"),!a.disableExceptionTracking){{f=\"onerror\",b(\"_\"+f);var g=e[f];e[f]=function(a,b,d,e,h){{var i=g&&g(a,b,d,e,h);return!0!==i&&c[\"_\"+f](a,b,d,e,h),i}}}}return c;}}({{instrumentationKey: \"{0}\"}});window.appInsights=appInsights,appInsights.queue&&0===appInsights.queue.length&&appInsights.trackPageView();";
 
         // GET: IKey
         public String Index()
         {
-            String ikey = Environment.GetEnvironmentVariable("APPLICATION_INSIGHTS_IKEY");
+            String ikey = Environment.GetEnvironmentVariable(ConfigurationManager.AppSettings["AppinsightInstrumentKeyName"]);
             if (ikey != null && ikey.Length != 0)
             {
-                return  String.Format(JavascriptInjectionPattern, ikey);
+                return String.Format(JavascriptInjectionPattern, ikey);
             }
             else
             {
