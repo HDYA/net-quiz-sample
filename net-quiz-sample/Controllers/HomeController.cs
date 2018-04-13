a52367a3d3434ce20979e3349fa03e168b089637
Chudi Huang <chuhua@microsoft.com>
2018-04-13 14:04:37 +0800

    Add redirect from root path

---
+++ net-quiz-sample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace net_quiz_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("<script>window.location = \"" + ConfigurationManager.AppSettings["IndexPagePath"] + "\"</script>");
+        }
+    }
+}
